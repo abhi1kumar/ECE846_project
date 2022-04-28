62ed83436ded6521a1d036a0bb2bb5200b3f9324
abhi1kumar <abhinav3663@gmail.com>
2022-04-27 20:15:15 -0400

    generate initializations for FON 3,4,8,16,32,64

--- matlab/main.m
+++ matlab/main.m
@@ -1,6 +1,6 @@
 options  = optimoptions('fmincon','display','none');
-num_init = 500;
-N   = 10;
+num_init = 500; % num of weighted sum
+N   = 10;  % num of repeats to be done
 
 
 fprintf("=============== FON 2 =====================\n")
@@ -9,4 +9,24 @@ x_init_matrix = FON(num_init, N, d, options);
 
 fprintf("=============== FON 3 =====================\n")
 d   = 3;
+x_init_matrix = FON(num_init, N, d, options);
+
+fprintf("=============== FON 4 =====================\n")
+d   = 4;
+x_init_matrix = FON(num_init, N, d, options);
+
+fprintf("=============== FON 8 =====================\n")
+d   = 8;
+x_init_matrix = FON(num_init, N, d, options);
+
+fprintf("=============== FON 16 =====================\n")
+d   = 16;
+x_init_matrix = FON(num_init, N, d, options);
+
+fprintf("=============== FON 32 =====================\n")
+d   = 32;
+x_init_matrix = FON(num_init, N, d, options);
+
+fprintf("=============== FON 64 =====================\n")
+d   = 64;
 x_init_matrix = FON(num_init, N, d, options);
