56cbf966ed00fa08ecce71020f2302302a45f860
abhi1kumar <abhinav3663@gmail.com>
2022-04-29 13:45:48 -0400

    main.sh added

--- main.sh
+++ main.sh
@@ -1,22 +1,33 @@
 # ==============================================================================
+# ZDT1
+# ==============================================================================
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT1 --dim 2
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT1 --dim 4
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT1 --dim 8
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT1 --dim 16
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT1 --dim 30 --save_gif
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT1 --dim 32
+
+# ==============================================================================
 # ZDT4
 # ==============================================================================
-python run_with_random_and_weighted_sum_init.py --dim 2 --problem_name ZDT4
-python run_with_random_and_weighted_sum_init.py --dim 3 --problem_name ZDT4
-python run_with_random_and_weighted_sum_init.py --dim 4 --problem_name ZDT4
-python run_with_random_and_weighted_sum_init.py --dim 8 --problem_name ZDT4
-python run_with_random_and_weighted_sum_init.py --dim 10 --problem_name ZDT4
-python run_with_random_and_weighted_sum_init.py --dim 16 --problem_name ZDT4
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT4 --dim 2
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT4 --dim 4
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT4 --dim 8
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT4 --dim 10 --save_gif
+python run_with_random_and_weighted_sum_init.py --problem_name ZDT4 --dim 16
 
 
 # ==============================================================================
 # FON
 # ==============================================================================
-python run_with_random_and_weighted_sum_init.py --dim 2
-python run_with_random_and_weighted_sum_init.py --dim 3
-python run_with_random_and_weighted_sum_init.py --dim 4
-python run_with_random_and_weighted_sum_init.py --dim 8
-python run_with_random_and_weighted_sum_init.py --dim 16
-python run_with_random_and_weighted_sum_init.py --dim 32
-python run_with_random_and_weighted_sum_init.py --dim 64
+python run_with_random_and_weighted_sum_init.py --problem_name FON --dim 2
+python run_with_random_and_weighted_sum_init.py --problem_name FON --dim 3 --save_gif
+python run_with_random_and_weighted_sum_init.py --problem_name FON --dim 4
+python run_with_random_and_weighted_sum_init.py --problem_name FON --dim 8
+python run_with_random_and_weighted_sum_init.py --problem_name FON --dim 16
+python run_with_random_and_weighted_sum_init.py --problem_name FON --dim 32
+python run_with_random_and_weighted_sum_init.py --problem_name FON --dim 64
 
+# Plot convergence plots
+python plot/plot_convergence.py
